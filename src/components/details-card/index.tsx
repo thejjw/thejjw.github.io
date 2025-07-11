35fc8ce1db3b98f1b5e62c5758dc97e5c65551a1
thejjw <72130076+thejjw@users.noreply.github.com>
2025-07-11 13:37:08 +0900

    Update index.tsx

--- src/components/details-card/index.tsx
+++ src/components/details-card/index.tsx
@@ -20,7 +20,7 @@ import {
 } from 'react-icons/fa';
 import { FaSquareThreads } from 'react-icons/fa6';
 import { MdLocationOn } from 'react-icons/md';
-import { RiDiscordFill, RiMailFill, RiPhoneFill } from 'react-icons/ri';
+import { RiMailFill, RiPhoneFill } from 'react-icons/ri'; // RiDiscordFill, 
 import { SiResearchgate, SiX, SiUdemy } from 'react-icons/si';
 import { Profile } from '../../interfaces/profile';
 import {
