dd2af72cea31b064dbd0b37f3c4a002b98017487
thejjw <72130076+thejjw@users.noreply.github.com>
2025-10-25 09:22:59 +0000

    Enhance compression tool UI with binary file support and update footer information

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -232,7 +232,7 @@ const CONFIG = {
   },
 
   // Optional Footer. Supports plain text or HTML.
-  footer: `based on GitProfile(arifszn/gitprofile). @thejjw 2025-2025`,
+  footer: `<span>page based on GitProfile(@arifszn/gitprofile). @thejjw 2025-2025</span><br><span><a href="./tool/compression-comparison-tool.html">tool 1: Compression Tool</a></span>`,
 
   enablePWA: true,
 };
